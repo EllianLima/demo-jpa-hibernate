0d82bc182cce482f9781a16aa6d8abeacbf6fb49
EllianLIma <elliancesar23@gmail.com>
2026-07-07 21:11:26 -0300

    Project created

---
+++ src/main/java/com/elliandemo/demo_jpa_hibernate/DemoJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.elliandemo.demo_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elliandemo/demo_jpa_hibernate/DemoJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elliandemo.demo_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
