84e1127fd35b70e81c6c953aaf66457ca942df08
EllianLIma <elliancesar23@gmail.com>
2026-07-15 10:46:34 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/elliandemo/demo_jpa_hibernate/entities/Product.java
+++ src/main/java/com/elliandemo/demo_jpa_hibernate/entities/Product.java
@@ -1,5 +1,6 @@
 package com.elliandemo.demo_jpa_hibernate.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -22,6 +23,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_products_categories", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){}
 
@@ -77,6 +81,16 @@ public class Product implements Serializable {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
+    }
+
 
     @Override
     public boolean equals(Object o) {
